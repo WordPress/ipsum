4607f77b049d273a0253107e5b31692b55af8d02
noruzzamans <noruzzamanrubel@gmail.com>
2026-09-22 11:17:32 +0600

    Update search bar pattern description

--- patterns/hidden-search-bar.php
+++ patterns/hidden-search-bar.php
@@ -3,7 +3,7 @@
  * Title: Search Bar
  * Slug: ipsum/hidden-search-bar
  * Inserter: no
- * Description: Sticky search bar button anchored at the bottom edge of the viewport.
+ * Description: A compact icon-only search button that expands into a search field when activated.
  *
  * @package Ipsum
  * @since Ipsum 1.0
